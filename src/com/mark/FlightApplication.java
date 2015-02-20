bbb71bf68fd9994b16b1ca73a43c36d3d67a6bf1
NeVeX <markcunningham9@gmail.com>
2015-02-20 02:34:29 -0800

    Working RestEasy endpoint in Tomcat with sanity check

---
+++ src/com/mark/FlightApplication.java
@@ -0,0 +1,9 @@
+package com.mark;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/flight")
+public class FlightApplication extends Application {
+
+}
---
+++ src/com/mark/controller/FlightController.java
@@ -0,0 +1,22 @@
+package com.mark.controller;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Path(value="/")
+public class FlightController {
+
+	@GET
+	@Path("/sanity")
+	public String testSanity()
+	{
+		return "Sanity Check Ok on "+LocalDateTime.now();
+	}
+	
+}
