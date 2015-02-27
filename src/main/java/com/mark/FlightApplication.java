37263f5855cf9d0addc80f1ee7799ad8a91fc01c
Mark Cunningham <mcunningham@prosper.com>
2015-02-26 19:06:01 -0800

    Feck RestEasy 3 and Spring 4 integration. Since I need views, I am going to use Spring 4 on its own for now...

--- src/main/java/com/mark/FlightApplication.java
+++ src/main/java/com/mark/FlightApplication.java
@@ -1,29 +1,29 @@
-package com.mark;
-
-import java.util.HashSet;
-import java.util.Set;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-import javax.ws.rs.core.Context;
-
-import org.jboss.resteasy.core.Dispatcher;
-
-import com.mark.controller.FlightController;
-
-public class FlightApplication extends Application {
-	private Set<Object> singletons = new HashSet<Object>();
-	 
-//    public FlightApplication(@Context Dispatcher dispatcher) {
-//        singletons.add(new FlightController(dispatcher.getDefaultContextObjects()));
+//package com.mark;
+//
+//import java.util.HashSet;
+//import java.util.Set;
+//
+//import javax.ws.rs.ApplicationPath;
+//import javax.ws.rs.core.Application;
+//import javax.ws.rs.core.Context;
+//
+//import org.jboss.resteasy.core.Dispatcher;
+//
+//import com.mark.controller.FlightController;
+//
+//public class FlightApplication extends Application {
+//	private Set<Object> singletons = new HashSet<Object>();
+//	 
+////    public FlightApplication(@Context Dispatcher dispatcher) {
+////        singletons.add(new FlightController(dispatcher.getDefaultContextObjects()));
+////    }
+//    
+//    public FlightApplication() {
+//    	singletons.add(new FlightController());
 //    }
-    
-    public FlightApplication() {
-    	singletons.add(new FlightController());
-    }
- 
-    @Override
-    public Set<Object> getSingletons() {
-        return singletons;
-    }
-}
+// 
+//    @Override
+//    public Set<Object> getSingletons() {
+//        return singletons;
+//    }
+//}
--- src/main/java/com/mark/controller/FlightController.java
+++ src/main/java/com/mark/controller/FlightController.java
@@ -3,34 +3,29 @@ package com.mark.controller;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-
-
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.mark.service.IFlightService;
 
 @Controller
-@Path(value="/")
 public class FlightController {
 
 	@Autowired
 	private IFlightService flightService;
 	
-	@GET
-	@Path("/sanity")
-	public String testSanity()
+	@RequestMapping(value="/sanity", method=RequestMethod.GET)
+	public @ResponseBody String testSanity()
 	{
 		return "Sanity Check OK\nTime: "+LocalDateTime.now();
 	}
 	
-	@GET
-	@Path("/hello")
-	public String hello()
+
+	@RequestMapping("/hello")
+	public @ResponseBody String hello()
 	{
 		return "Computer says: "+flightService.sayHi()+"\nTime: "+LocalDateTime.now();
 	}
