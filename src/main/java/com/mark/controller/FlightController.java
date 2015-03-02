5d72ba58065c5259a534c30d689e9ff84f07363b
NeVeX <markcunningham9@gmail.com>
2015-03-01 22:15:07 -0800

    Exposing test data through API and making the output work through GAE

--- src/main/java/com/mark/controller/FlightController.java
+++ src/main/java/com/mark/controller/FlightController.java
@@ -8,10 +8,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.mark.model.google.response.GoogleFlightResponse;
 import com.mark.service.IFlightService;
 
-@Controller
+@RestController
 public class FlightController {
 
 	@Autowired
@@ -24,14 +26,20 @@ public class FlightController {
 	}
 	
 	@RequestMapping("/hello/{name}")
-	public @ResponseBody String hello(@PathVariable("name") String name)
+	public String hello(@PathVariable("name") String name)
 	{
 		return flightService.sayHi(name)+"<br><br>Time: "+new Date();
 	}
 	
 	@RequestMapping("/hello")
-	public @ResponseBody String hello()
+	public String hello()
 	{
 		return "Hi!<br><br>Time: "+new Date();
+	}
+	
+	@RequestMapping(value="/info/{flight}", /*headers="Accept=application/json",*/ produces = {"application/json"})
+	public GoogleFlightResponse getFlightDetails(@PathVariable("flight") String flightInfo)
+	{
+		return flightService.getFlights(flightInfo);
 	}
 }
